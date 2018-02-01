877864ef4a01a4b6be47d44040c0410a247d88fd
wangzy0327 <wangzy0327@qq.com>
2018-02-01 10:50:04 +0800

    add user scan timing

--- src/main/java/com/task/util/HttpUnitHelper.java
+++ src/main/java/com/task/util/HttpUnitHelper.java
@@ -36,11 +36,20 @@ public class HttpUnitHelper {
             //模拟浏览器打开一个目标网址
             HtmlPage htmlPage = wc.getPage(url);
             //为了获取js执行的数据 线程开始沉睡等待
-            Thread.sleep(1000);//这个线程的等待 因为js加载需要时间的
+            Thread.sleep(1500);//这个线程的等待 因为js加载需要时间的
             //以xml形式获取响应文本
             String xml = htmlPage.asXml();
             //并转为Document对象return
             return Jsoup.parse(xml);
+//            return Jsoup.connect(url).get();
+//            return Jsoup.connect(url)
+//                    .userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.64 Safari/537.31")
+//                    .get();
+//            return Jsoup.connect(url).data("query", "Java")
+//                    .userAgent("Mozilla")
+//                    .cookie("auth", "token")
+//                    .timeout(3000)
+//                    .post();
             //System.out.println(xml.contains("结果.xls"));//false
         } catch (FailingHttpStatusCodeException e) {
             e.printStackTrace();
